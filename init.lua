ca844a4ba2c1fea08c0eba63316cb67d3ac815c0
MistUnky <misterunknown128@gmail.com>
2021-09-06 19:21:52 -0400

    add awlib namespace for functions - makes code cleaner

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
-function reg_plont(plant, soils, atmos)
+awlib = {}
+
+function awlib.reg_plont(plant, soils, atmos)
 	minetest.register_abm({
 		nodenames = soils,
 		interval = 1,
@@ -21,11 +23,11 @@ function reg_plont(plant, soils, atmos)
 	})
 end
 
-function reg_plant(plant, soils)
-	reg_plont(plant, soils, "air")
+function awlib.reg_plant(plant, soils)
+	awlib.reg_plont(plant, soils, "air")
 end
 
-function reg_parasite(parasite, food, ch)
+function awlib.reg_parasite(parasite, food, ch)
 	minetest.register_abm({
 		nodenames = food,
 		neighbors = {parasite}, -- this instead of 2?
@@ -40,7 +42,7 @@ function reg_parasite(parasite, food, ch)
 	})
 end
 
-function reg_finite_liq(liquid)
+function awlib.reg_finite_liq(liquid)
 	minetest.register_abm({
 		nodenames = {liquid.."_source"},
 		interval = 1,
@@ -73,20 +75,20 @@ function reg_finite_liq(liquid)
 	minetest.override_item(liquid.."_flowing", {liquid_renewable = false})
 end
 
-function reg_sync_f(brain)
+function awlib.reg_sync_f(brain)
 	for node = 1, #brain do
 		minetest.override_item(brain[node], {walkable = true, groups = {falling_node = 1, choppy = 1}})
-		reg_parasite(brain[node], brain, 28)
+		awlib.reg_parasite(brain[node], brain, 28)
 	end
 end
 
-function reg_sync(brain)
+function awlib.reg_sync(brain)
 	for node = 1, #brain do
-		reg_parasite(brain[node], brain, 28)
+		awlib.reg_parasite(brain[node], brain, 28)
 	end
 end
 
-function reg_melt(solid) --needs work
+function awlib.reg_melt(solid) --needs work
 	minetest.register_abm({
 		nodenames = {solid},
 		interval = 1,
@@ -131,7 +133,7 @@ function reg_melt(solid) --needs work
 	})
 end
 
-function reg_fall(nodee)
+function awlib.reg_fall(nodee)
 	minetest.register_abm({
 		nodenames = {nodee},
 		interval = 1,
@@ -151,30 +153,30 @@ function reg_fall(nodee)
 end
 
  -- example use - minetest_game active world - makes mossycobble spread, finite liquids, land erosion, and coral reef growth (add this to example.lua (?) or make this a modpack and have this be example mod (?))
-reg_parasite("air", {"default:snow"}, 1) -- erase snow layers, default:snow messes up when combined with the terrain melting function
-reg_parasite("default:mossycobble", {"default:cobble"}, 28)
-reg_finite_liq("default:water")
-reg_finite_liq("default:lava")
-reg_finite_liq("default:river_water")
-reg_melt("group:soil") --melt soils
-reg_melt("group:sand") -- melt sands
-reg_melt("default:gravel") --melt gravel
-reg_melt("default:snowblock") --melt snowblock
-reg_fall("default:cactus") --plants, trees, bushes and flora fall down after terrain melting/erosion
-reg_fall("default:papyrus")
-reg_fall("group:tree")
+awlib.reg_parasite("air", {"default:snow"}, 1) -- erase snow layers, default:snow messes up when combined with the terrain melting function
+awlib.reg_parasite("default:mossycobble", {"default:cobble"}, 28)
+awlib.reg_finite_liq("default:water")
+awlib.reg_finite_liq("default:lava")
+awlib.reg_finite_liq("default:river_water")
+awlib.reg_melt("group:soil") --melt soils
+awlib.reg_melt("group:sand") -- melt sands
+awlib.reg_melt("default:gravel") --melt gravel
+awlib.reg_melt("default:snowblock") --melt snowblock
+awlib.reg_fall("default:cactus") --plants, trees, bushes and flora fall down after terrain melting/erosion
+awlib.reg_fall("default:papyrus")
+awlib.reg_fall("group:tree")
 --BUSHES
-reg_fall("default:bush_leaves")
-reg_fall("default:acacia_bush_leaves")
-reg_fall("default:pine_bush_leaves")
-reg_fall("default:blueberry_bush_leaves")
-reg_fall("default:blueberry_bush_leaves_with_berries")
-reg_fall("group:flora")
+awlib.reg_fall("default:bush_leaves")
+awlib.reg_fall("default:acacia_bush_leaves")
+awlib.reg_fall("default:pine_bush_leaves")
+awlib.reg_fall("default:blueberry_bush_leaves")
+awlib.reg_fall("default:blueberry_bush_leaves_with_berries")
+awlib.reg_fall("group:flora")
 --underwatercorals
-reg_plont("default:coral_skeleton", {"default:sand"}, "default:water_source")
-reg_plont("default:coral_brown", {"default:sand"}, "default:water_source")
-reg_plont("default:coral_orange", {"default:sand"}, "default:water_source")
-reg_plont("default:coral_cyan", {"default:sand"}, "default:water_source")
-reg_plont("default:coral_green", {"default:sand"}, "default:water_source")
-reg_plont("default:coral_pink", {"default:sand"}, "default:water_source")
---reg_plont("default:sand_with_kelp", {"default:sand"}, "default:water_source") --broken :( (*This is because of how kelp is implemented)
+awlib.reg_plont("default:coral_skeleton", {"default:sand"}, "default:water_source")
+awlib.reg_plont("default:coral_brown", {"default:sand"}, "default:water_source")
+awlib.reg_plont("default:coral_orange", {"default:sand"}, "default:water_source")
+awlib.reg_plont("default:coral_cyan", {"default:sand"}, "default:water_source")
+awlib.reg_plont("default:coral_green", {"default:sand"}, "default:water_source")
+awlib.reg_plont("default:coral_pink", {"default:sand"}, "default:water_source")
+--awlib.reg_plont("default:sand_with_kelp", {"default:sand"}, "default:water_source") --broken :( (*This is because of how kelp is implemented)
